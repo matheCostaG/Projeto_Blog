1adab444bf2a8db7bbf0066fc3a882a4adb963ab
MayquelPhilipe <mayquelphilipe@gmail.com>
2017-09-09 16:48:08 -0300

    modific index

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
 <?php
 	require_once "classes/config.php";
-<<<<<<< HEAD
-
-
-
-	$conn = new mysqli("localhost", "root", "", "blog");
-=======
 	require_once "includes/conexao.php";
->>>>>>> bb0a55b300b1a50d7a644fd2742bcff854f80596
   $result = $conn->query("select * from admin as a join publicacao as p on a.id = p.id_admin order by p.id DESC");
   while ($row = $result->fetch_assoc()){
-<<<<<<< HEAD
     echo "<div class='divcenter'>";
-    echo "<ul class='boxposts'>";
-    echo "<li class='list-group-item' >";
-    echo "<span class='thumb'>";
-    echo "<img src='imagens/".$row['img_postagem']."' alt='' title='' width='166' height='166'>"; 
-    echo "</span>";
+    echo "<ul class='boxposts '  style=' background-size: 100%  auto; background-image: url(imagens/".$row['img_postagem']." );>";
+    echo "<li>";
     echo "<span class='content'>";
     echo "<h1>".$row['titulo']."</h1>";
-    echo "<p>".$row['texto']."</p>";
     echo "<div class='footer_post'>";
-    echo "<a href='#'> Leia o Artigo Completo</a>";            
-    echo "<span class'datapost float-right'>Data de Publicação: <strong>".$row['data']."</strong></span>";
+    echo "<a href='#'> Leia o Artigo Completo </a>";            
+    echo "<span class'datapost'> Data de Publicação: <strong>".$row['data']."</strong></span>";
     echo "</div>";
     echo "</span>";
     echo "</li>";
@@ -31,21 +19,9 @@
   
 }
 
-
-
-
-=======
-  echo "<img src='imagens/".$row['img_postagem']."'>";
-  echo $row['nome'];
-  echo $row['titulo'];
-  echo $row['texto'];
-  echo $row['data'];
-  }
-
 	?>
 
 
 	<?php
->>>>>>> bb0a55b300b1a50d7a644fd2742bcff854f80596
 	require_once "includes/footer.php";
  ?>
