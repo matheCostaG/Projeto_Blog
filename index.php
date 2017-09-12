dbb0f4f45d1fff468c84814a81c8d00681a89b66
MatheCostaG <Matheuscosta12535@gmail.com>
2017-09-12 03:50:37 -0300

    Terminando o index

--- index.php
+++ index.php
@@ -2,33 +2,27 @@
 	require_once "includes/header.php";
 	require_once "includes/conexao.php";
 
-  ?>
 
-  <?php
-  $result = $conn->query("select * from admin as a join publicacao as p on a.id = p.id_admin order by p.data DESC");
-
-  while ($row = $result->fetch_assoc()){
-
-    echo "<div class='divcenter text-center'>";
+  $result = $conn->query("select p.id, titulo, img_postagem, data, nome, MID(texto,1,250) from admin as a join publicacao as p on a.id = p.id_admin order by p.data DESC");
+	$i = 0;
+	while ($row = $result->fetch_assoc()){
+    echo "<div class='divcenter '>";
     echo "<ul class='boxposts'  style=' background-size: 100%  auto; background-image: url(imagens/".$row['img_postagem']." );>";
     echo "<li class='list-group-item' >";
-    echo "<span class='content'>";
+    echo "<span class='teste content'>";
     echo "<div class='fundo-titulo'>";
-    echo "<a href='postagem.php'><h1>".$row['titulo']."</h1></a>";
-    echo "<div class='footer_post'>";
-    echo "<strong>".$row['texto']."</strong>";
-    echo "<span class='datapost'>Data de Publicação: <strong>".$row['data']."</strong></span>";
-    echo "</div>";
+		echo "<span class='datapost'>".$row['data']."</span>";
+    echo "<a class='text-center ' href='postagem.php'><h1>".$row['titulo']."</h1></a>";
+    echo "<div class='abrev_texto text-center texto_valor'>".$row['MID(texto,1,250)']."...</div>";
+		echo "<span class='datapost'>".$row['nome']."</span>";
     echo "</div>";
     echo "</span>";
     echo "</li>";
     echo "</ul>";
     echo "</div>";
+
 }
 $conn->close();
 
-	?>
-
-	<?php
 	require_once "includes/footer.php";
  ?>
